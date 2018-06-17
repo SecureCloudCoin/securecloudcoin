634591d7ad126d0bb356531a2dfb632a505937d0
albertcrombwie <admin@securecloudcoin.com>
2018-06-17 15:09:14 +0200

    changed versioning

--- src/version.h
+++ src/version.h
@@ -11,18 +11,17 @@
 /**
  * network protocol versioning
  */
-
-static const int PROTOCOL_VERSION = 70008;
+static const int PROTOCOL_VERSION = 74008;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 70000;
 
 //! In this version, 'getheaders' was introduced.
 static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70008;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70008;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 74008;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 74008;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
