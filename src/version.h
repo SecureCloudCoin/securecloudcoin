2620e1a98cf623fd5c0d0e36f755bcabd6013da3
albertcrombwie <admin@securecloudcoin.com>
2018-06-17 22:51:15 +0200

    version

--- src/version.h
+++ src/version.h
@@ -11,17 +11,17 @@
 /**
  * network protocol versioning
  */
-static const int PROTOCOL_VERSION = 82499;
+static const int PROTOCOL_VERSION = 72499;
 
 //! initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 70000;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 82498;
+static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 82498;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 82498;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 72499;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 72499;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -34,7 +34,7 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 82498;
+static const int NO_BLOOM_VERSION = 70000;
 
 
 #endif // BITCOIN_VERSION_H
